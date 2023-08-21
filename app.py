441f13cf24c8cca8fa4d481e810c5220c8ced70c
bhavya1804 <nairbhavya1804@gmail.com>
2023-08-21 08:11:42 -0400

    newimagetest

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from fastapi import FastAPI
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Load the Dolly v2 3B model
+# Load the model and tokenizer
 model_name = "databricks/dolly-v2-3b"
-model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
-@app.post("/generate/")
+@app.post("/generate")
 async def generate_prompt(prompt: str):
-    # Tokenize and generate text
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
-    output = model.generate(input_ids, max_length=100)
-
-    # Decode and return generated text
+    with torch.no_grad():
+        output = model.generate(input_ids, max_length=100)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return {"generated_text": generated_text}
